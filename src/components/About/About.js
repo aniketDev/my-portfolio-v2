fc8792f12f7a02cf59e48caec3fdba258541aa0d
Aniket Mandal <aniketmandal9@gmail.com>
2019-12-25 16:04:59 +0530

    Section header

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,27 +1,23 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+import './About.scss';
 
 const About = () => {
   return (
-    <Grid container className="about" alignItems="center" alignContent="center">
-      <div className="about__heading section-heading">
+    <Grid container className="about">
+      <Grid item xs={12} className="section-heading">
         <h1>
-          About Me<span>Let Me Introduce</span>
+          About Me
+          {/* <span>Let Me Introduce</span> */}
         </h1>
-      </div>
-      <div className="about__images">
-        <div className="image__container">
-          <img src="/assets/images/img1.jpg" alt="" className="img--1" data-aos="fade-right" />
-          <img src="/assets/images/img2.jpg" alt="" className="img--2" data-aos="fade-left" />
-          <img src="/assets/images/img3.jpg" alt="" className="img--3" data-aos="fade-up" />
-        </div>
-      </div>
-      <div className="about__info" data-aos="fade-up">
-        <p className="paragraph">
+      </Grid>
+
+      <Grid item xs={12} className="section-content about__content">
+        <p>
           Hi there, I am Aniket Mandal. I am a professional front-end web developer with 1+ years of
           professional experience.
         </p>
-        <p className="paragraph">
+        <p>
           After passing my M.Tech I started my job as a Project Coordinator in a company named IUMS
           Pvt. Ltd. back in Kolkata. There I find my passion for web development. Soon I changed my
           career and came all the way to Bangalore. Here I started my job as a Front-end Web
@@ -30,14 +26,14 @@ const About = () => {
           designing and developing front end part of the entire application, I learned a lot of
           things by myself and became a passionate web developer.
         </p>
-        <p className="paragraph">
+        <p>
           I try to learn new things each and every day and I feel a day without learning anything
           new is a day wasted.
         </p>
         <a href="https://drive.google.com/uc?authuser=0&id=1W3hs08YNID8hJLBdFikxskHGN769rceY&export=download">
           <button className="download">Download CV</button>
         </a>
-      </div>
+      </Grid>
     </Grid>
   );
 };
