0cbbe922b28e87389b93833c7837ee7af0f8376a
Aniket Mandal <aniketmandal9@gmail.com>
2020-02-02 05:05:11 +0530

    Improve home and about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,31 +14,37 @@ const About = () => {
         </h1>
       </Grid>
 
-      <Grid item xs={12} className="section-content about__content">
-        <Typography variant="inherit" component="p">
-          Hi there! I'm Aniket Mandal. I'm a professional <strong>Frontend Developer</strong> with
-          <strong> 2+ years</strong> of professional experience.
-        </Typography>
-        <Typography variant="inherit" component="p">
-          After passing my M.Tech I started my job as a Project Coordinator in a company named
-          Innova Unified Management Services Pvt. Ltd. in Kolkata where I found my passion for web
-          development. Soon I decided to change my career and came to Bangalore. Here I started my
-          job as a Frontend Web Developer in a startup named Balihans Software Pvt. Ltd. I started
-          designing and developing the company's own product called Qnabu. As I am the only one
-          responsible for designing and developing front end part of the entire application, I
-          learned a lot of things by myself and became a passionate web developer. After finishing
-          the project and deploying it in production I was looking for working in different domains
-          and started working in Primesoft IP Solutions Pvt. Ltd. as a Software Engineer. I'm
-          currently working in this organization for almost 1 year. During this time period I've
-          worked there in a banking project and other projects using various tools and technologies
-          like Angular 5, Ionic 3, React js and Backbone js.
-        </Typography>
-        <Typography variant="inherit" component="p">
-          I try to learn new things each and every day and I feel a day without learning something
-          new is a day wasted.
-        </Typography>
+      <Grid container item xs={12} className="about__content">
+        <Grid item xs={7} className="about__content__text">
+          <Typography variant="inherit" component="p">
+            <span>&#8220;</span>
+            Hi there! I'm Aniket Mandal. I'm a professional <strong>Frontend Developer</strong> with
+            <strong> 2+ years</strong> of professional experience.
+          </Typography>
+          <Typography variant="inherit" component="p">
+            After passing my M.Tech I started my job as a Project Coordinator in a company named
+            Innova Unified Management Services Pvt. Ltd. in Kolkata where I found my passion for web
+            development. Soon I decided to change my career and came to Bangalore. Here I started my
+            job as a Frontend Web Developer in a startup named Balihans Software Pvt. Ltd. I started
+            designing and developing the company's own product called Qnabu. As I am the only one
+            responsible for designing and developing front end part of the entire application, I
+            learned a lot of things by myself and became a passionate web developer. After finishing
+            the project and deploying it in production I was looking for working in different
+            domains and started working in Primesoft IP Solutions Pvt. Ltd. as a Software Engineer.
+            I'm currently working in this organization for almost 1 year. During this time period
+            I've worked there in a banking project and other projects using various tools and
+            technologies like Angular 5, Ionic 3, React js and Backbone js.
+          </Typography>
+          <Typography variant="inherit" component="p">
+            I try to learn new things each and every day and I feel a day without learning something
+            new is a day wasted.
+          </Typography>
 
-        <button className="download">Download CV</button>
+          <button className="download">Download CV</button>
+        </Grid>
+        <Grid item xs={5} className="about__content__image">
+          <div className="profile-image"></div>
+        </Grid>
       </Grid>
     </Grid>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,17 +2,20 @@ import React from 'react';
 import { Grid, Button } from '@material-ui/core';
 import './Home.scss';
 
+const homeImage = require('../../assets/images/OBJECTS.png');
+
 const Home = () => {
   return (
     <Grid container className="home" alignItems="center" alignContent="center">
-      <Grid item xs={7}>
+      <Grid item xs={12}>
+        <img class="home-image" src={homeImage} alt="a"></img>
         <div className="home-text-container">
           <h1>
             <span>Hello, I am</span>
             <span>Aniket Mandal</span>
           </h1>
           <p>
-            I'm a frontend web developer currently based in Bangalore <br />
+            I'm a frontend web developer currently based in Bangalore, <br />
             focused on writing clean, elegant, efficient and functional code.
           </p>
           <Button size="large" variant="contained" className="home-button">
@@ -20,9 +23,9 @@ const Home = () => {
           </Button>
         </div>
       </Grid>
-      <Grid item xs={5} className="profile-image-container">
-        <div className="profile-image">{/* <img src={profileImage} alt="profile-image" /> */}</div>
-      </Grid>
+      {/* <Grid item xs={5} className="profile-image-container">
+        <div className="profile-image"></div>
+      </Grid> */}
     </Grid>
   );
 };
